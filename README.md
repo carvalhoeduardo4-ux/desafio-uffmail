7e0be19c1e37fe084c12df2a52885548c1679db1
carvalhoeduardo4-ux <carvalhoeduardo4@gmail.com>
2026-03-09 16:25:08 -0300

    Entrega do desafio de criação de UFFMail
    Implementação da lógica de criação de e-mails em Python, com leitura/escrita de CSV e aplicação de conceitos de Orientação a Objetos.

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# desafio-uffmail
+# Sistema de Criação de UFFMail - Desafio Técnico 🎓
+
+Este projeto é uma solução para o desafio de implementação da criação de contas de e-mail (UFFMail) para alunos da Universidade Federal Fluminense. O sistema processa dados de um arquivo CSV, valida o status do aluno e gera opções personalizadas de e-mail.
+
+##  Funcionalidades
+
+- **Leitura de Dados:** Consumo de base de dados via arquivo `.csv`.
+- **Validação de Regras de Negócio:**
+  - Apenas alunos com status **Ativo** podem criar e-mails.
+  - Alunos que já possuem um UFFMail cadastrado são impedidos de criar um novo.
+- **Gerador de Opções:** Algoritmo que gera 4 variações de e-mail baseadas no nome do aluno.
+- **Persistência de Dados:** O sistema atualiza o arquivo CSV automaticamente após a escolha do usuário.
+- **Simulação de Notificações:** Exibição de confirmação de criação de conta e envio de SMS com senha.
+
+##  Tecnologias e Conceitos Utilizados
+
+- **Linguagem:** Python 
+- **Orientação a Objetos (OO):** Utilização de classes (`Aluno`, `SistemaUFF`, `GeradorUffMail`) para garantir baixo acoplamento e alta coesão.
+- **Manipulação de Arquivos:** Uso das bibliotecas nativas `csv` para leitura e escrita (`DictReader` e `DictWriter`).
+- **Tratamento de Exceções:** Robustez no código para lidar com erros de entrada de dados ou arquivos ausentes.
+
+##  Estrutura do Código
+
+- **Aluno:** Classe modelo que representa a entidade aluno.
+- **GeradorUffMail:** Classe utilitária com métodos estáticos para lógica de strings.
+- **SistemaUFF:** Classe controladora responsável pelo fluxo principal e interação com o arquivo CSV.
---
+++ main.py
@@ -0,0 +1,114 @@
+import csv
+
+class Aluno:
+    def __init__(self,nome,matricula,telefone,email,uffmail,status):
+        self.nome = nome
+        self.matricula = matricula
+        self.telefone = telefone
+        self.email = email
+        self.uffmail = uffmail
+        self.status = status
+        
+    def is_ativo(self):
+        return self.status.lower() == 'ativo'
+    
+    
+class GeradorUffMail:
+    @staticmethod
+    def gerar_opcoes(nome_completo):
+        partes = nome_completo.lower().split() #transforma os nomes em minusculo e faz uma lista onde cada nome da pessoa é uma posição
+        primeiro_nome = partes[0] 
+        ultimo_nome = partes[-1] 
+        
+        opcoes = [
+            f"{primeiro_nome}_{ultimo_nome}@id.uff.br",
+            f"{primeiro_nome[0]}_{ultimo_nome}@id.uff.br",
+            f"{primeiro_nome}_{ultimo_nome[0]}@id.uff.br",
+            f"{primeiro_nome[0]}{primeiro_nome}{ultimo_nome[0]}{ultimo_nome}@id.uff.br"
+
+        ]
+        return opcoes
+
+
+class SistemaUFF:
+    def __init__(self,caminho_csv):
+        self.caminhos_csv = caminho_csv
+        self.alunos = {}
+        self.carregar_dados(caminho_csv)
+    
+    def carregar_dados(self,caminho_csv):
+        try:
+            with open(caminho_csv,mode ='r',encoding='utf-8') as arquivo:
+                leitor = csv.DictReader(arquivo) #lê o CSV e entrega cada linha como um dicionário
+                for linha in leitor:
+                    aluno = Aluno(
+                        linha['nome'],
+                        linha['matricula'],
+                        linha['telefone'],
+                        linha['email'],
+                        linha['uffmail'],
+                        linha['status']
+                    )
+                    self.alunos[aluno.matricula] = aluno
+        except FileNotFoundError:
+            print('ERRO: Arquivo csv não encontrado.')
+    def atualiza_csv(self):
+        colunas = ['nome','matricula','telefone','email','uffmail','status']
+        try:
+            with open(self.caminhos_csv,mode = 'w', encoding='utf-8') as arquivo:
+                escritor = csv.DictWriter(arquivo, fieldnames=colunas) #recebe um dicionário e o transforma em uma linha de CSV, seguindo a ordem das fieldnames (colunas)
+                escritor.writeheader() #escreve o cabeçalho
+                for aluno in self.alunos.values():
+                    escritor.writerow({
+                        'nome' : aluno.nome,
+                        'matricula' : aluno.matricula,
+                        'telefone' : aluno.telefone,
+                        'email' : aluno.uffmail,
+                        'status' : aluno.status
+                    })
+        except Exception:
+            print(f'Erro ao salvar arquivo {Exception}')
+    
+    def processar_solicitacao(self):
+        matricula = input('Entre com a matricula: ')
+        
+        if not matricula in self.alunos:
+            print('Matricula não encontrada')
+            return
+        aluno = self.alunos[matricula]
+        
+        #Regra 1: apenas alunos ativos podem ter um UFFmail
+        if not aluno.is_ativo():
+            print("Apenas alunos com status 'ativo' podem criar um UFFmail")
+            return
+        
+        #Regra 2: um aluno só pode ter um UFFmail
+        if aluno.uffmail:
+            print('O aluno ja possui um UFFmail cadastrado')
+            return
+        
+        #Gerando opcoes
+        print(f'\n{aluno.nome.split()[0]} por favor, escolha uma das opcoes abaixo:')
+        opcoes = GeradorUffMail.gerar_opcoes(aluno.nome)
+        
+        for i,opcao in enumerate(opcoes,1):
+            print(f'{i} - {opcao}')
+        
+        try:
+            escolha = int(input('Digite o numero da opcao desejada: '))
+            email_escolhido = opcoes[escolha - 1]
+            
+            aluno.uffmail = email_escolhido
+            self.atualiza_csv()
+            
+            print(f"\nA criação de seu e-mail ({email_escolhido}) será feita nos próximos minutos.")
+            print(f"Um SMS foi enviado para {aluno.telefone} com a sua senha de acesso.")
+        
+        except (ValueError, IndexError):
+            print("Opção inválida.")
+    
+    
+if __name__ == "__main__":
+    sistema = SistemaUFF('alunos.csv')
+    sistema.processar_solicitacao()
+        
